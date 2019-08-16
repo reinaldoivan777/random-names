50108cbe010a3005736698a18a85c9e77d95e984
Ivan Reinaldo <reinaldoivan777@gmail.com>
2019-08-16 12:16:57 +0700

    random name first init

---
+++ src/Components/Form.js
@@ -0,0 +1,47 @@
+import React, {Component, Fragment} from "react";
+import { Input, Button, Container } from "reactstrap";
+import Result from './Result'
+
+class Form extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            names:'',
+            result: ''
+        }
+    }
+
+    randomize(max) {
+        return Math.floor(Math.random() * Math.floor(max));
+    }
+
+    handleRandom = () => {
+        const {names} = this.state
+        let listName = names.split(" ")
+        
+        this.setState({
+            result: listName[this.randomize(listName.length)]
+        })
+    }
+
+    handleChange = (e) => {
+        this.setState({
+            names: e.target.value
+        })
+    }
+
+    render() {
+        const {result} = this.state
+        return(
+            <Fragment>
+                <Container>
+                <Input type='textarea' className='form-control mb-3' placeholder='Enter Names' onChange={(e) => this.handleChange(e)} />
+                <Button className="mb-3" color='primary' onClick={() => this.handleRandom()}>Click to Random</Button>
+                </Container>
+                {result && <Result result={result} />}
+            </Fragment>
+        )
+    }
+}
+
+export default Form
---
+++ src/Components/Result.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Alert } from "reactstrap";
+
+const Result = ({result}) => (
+    <Alert color='primary'>{result}</Alert>
+)
+
+export default Result
